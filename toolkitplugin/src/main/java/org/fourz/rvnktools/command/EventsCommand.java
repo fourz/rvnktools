e3efac6d825ef5dd581c6ff3356e6b6f5f27e5e0
Brian Trout <btrout@uif.uillinois.edu>
2024-09-28 09:50:11 -0500

    Organize project folder - Add command folder - Add listener folder - Move commands and listeners to folders - Move AnnouncementManager.java

--- toolkitplugin/src/main/java/org/fourz/rvnktools/command/EventsCommand.java
+++ toolkitplugin/src/main/java/org/fourz/rvnktools/command/EventsCommand.java
@@ -1,4 +1,4 @@
-package org.fourz.rvnktools;
+package org.fourz.rvnktools.command;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
--- toolkitplugin/src/main/java/org/fourz/rvnktools/listener/JoinListener.java
+++ toolkitplugin/src/main/java/org/fourz/rvnktools/listener/JoinListener.java
@@ -1,13 +1,12 @@
-package org.fourz.rvnktools;
+package org.fourz.rvnktools.listener;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-
 public class JoinListener implements Listener {
-	
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
