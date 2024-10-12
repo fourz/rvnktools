0b37d934b077b2d2b1c63294b7e5c96c9f6289ff
Derek <dev@fourz.org>
2024-10-12 10:15:57 -0500

    removed .vscode files from derekdev

--- .vscode/copyto-server-DEV.ps1
+++
@@ -1,3 +0,0 @@
-
-# Copy the plugin to the server
-Copy-Item -Path 'C:\tools\rvnktools\toolkitplugin\target\rvnktools-1.0-SNAPSHOT.jar' -Destination 'F:\Minecraft\MCSS\servers\RVNK Dev\plugins' -Force
